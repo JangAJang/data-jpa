4b7d9c0ccd728d3f8ed8272895b56ac5b079b429
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-29 17:29:15 +0900

    feat: MemberTestRepository CRUD를 수행하는 리포지토리 메서드 구현

--- src/main/java/com/study/datajpa/repository/MemberTestRepository.java
+++ src/main/java/com/study/datajpa/repository/MemberTestRepository.java
@@ -1,10 +1,15 @@
 package com.study.datajpa.repository;
 
 import com.study.datajpa.domain.Member;
+import com.study.datajpa.domain.Team;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Repository
@@ -13,14 +18,39 @@ public class MemberTestRepository {
     @PersistenceContext
     private EntityManager em;
 
+    //Create
     public Member saveMember(Member member){
         em.persist(member);
         return member;
     }
 
-    public Member findMember(Long id){
-        return em.find(Member.class, id);
+    //Read One
+    public Optional<Member> findMember(Long id){
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
     }
+
+    //Read All
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    //Read Count
+    public Long countMember(){
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
+    }
+
+    //Update
+    public Member updateMember(String name, int age, Team team){
+        return null;
+    }
+
+    //Delete
+    public void deleteMember(Long id){
+        em.remove(em.find(Member.class, id));
+    }
+
+
 
 
 }
