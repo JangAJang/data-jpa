30ee9b181011bd69c1664588000b5b561f1d1c18
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-29 19:46:30 +0900

    Feat: MemberRepositoryTest JPQL로 만든 리포지토리와 JpaRepository를 확장해 받은 인터페이스 리포지토리를 비교하기 위한 테스트코드 구현

--- src/test/java/com/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/study/datajpa/repository/MemberRepositoryTest.java
@@ -2,6 +2,7 @@ package com.study.datajpa.repository;
 
 import com.study.datajpa.domain.Member;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,5 +23,52 @@ class MemberRepositoryTest {
         memberRepository.save(member);
         Member find = memberRepository.findById(member.getId()).get();
         Assertions.assertThat(find).isEqualTo(member);
+    }
+
+    @Test
+    @DisplayName("Create Test")
+    public void basicC(){
+        Member member = new Member("테스트");
+        Member saved = memberRepository.save(member);
+        Assertions.assertThat(saved).isEqualTo(member);
+    }
+
+    @Test
+    @DisplayName("Read All Test")
+    public void basicR_All() throws Exception{
+        //given
+        Member member1 = new Member("멤버1");
+        Member member2 = new Member("멤버2");
+        //when
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //then
+        Assertions.assertThat(memberRepository.findAll()).contains(member1, member2);
+    }
+
+    @Test
+    @DisplayName("Update Test")
+    public void basicU() throws Exception{
+        //given
+        Member member = new Member("테스트");
+        memberRepository.save(member);
+        //when
+        String changedName = "변경명";
+        member.changeUsername(changedName);
+        Member findMember = memberRepository.save(member);
+        //then
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(changedName);
+    }
+
+    @Test
+    @DisplayName("Delete Test")
+    public void basicD() throws Exception{
+        //given
+        Member member = new Member("삭제용");
+        memberRepository.save(member);
+        //when
+        memberRepository.delete(member);
+        //then
+        Assertions.assertThat(memberRepository.findAll()).doesNotContain(member);
     }
 }
