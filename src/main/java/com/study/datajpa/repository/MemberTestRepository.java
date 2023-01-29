aca396aca60ba8dcfe8be8d94b4ea9daf11fc221
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-29 20:24:24 +0900

    feat: MemberTestRepository JPQL을 이용한 페이징 조회 기능 구현

--- src/main/java/com/study/datajpa/repository/MemberTestRepository.java
+++ src/main/java/com/study/datajpa/repository/MemberTestRepository.java
@@ -59,4 +59,21 @@ public class MemberTestRepository {
                 ).setParameter("username", username).setParameter("age", age)
                 .getResultList();
     }
+
+    public List<Member> findByPage(int age, int offset, int limit){
+        return em.createQuery(
+                "select m from Member m " +
+                        "where m.age = :age " +
+                        "order by m.username desc", Member.class
+        ).setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public Long countTotal(int age, int offset, int limit){
+        return em.createQuery("select count(m) from Member m " + "where m.age = :age ", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
 }
