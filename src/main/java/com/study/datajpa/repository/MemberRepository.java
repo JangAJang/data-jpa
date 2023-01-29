1851b75df5b39fb9a722392b899f57a684a5c8fb
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-29 15:14:26 +0900

    feat: MemberRepository 엔티티 멤버를 데이터베이스에 저장하는 기능 구현

---
+++ src/main/java/com/study/datajpa/repository/MemberRepository.java
@@ -0,0 +1,22 @@
+package com.study.datajpa.repository;
+
+import com.study.datajpa.entity.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Member saveMember(Member member){
+        em.persist(member);
+        return member;
+    }
+
+
+}
