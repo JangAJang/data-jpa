c1a51c6d9ccf518f99737e4206ba0b8bc01aaad2
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-29 15:37:36 +0900

    feat: MemberRepository 인터페이스와 JpaRepository확장을 이용한 리포지토리와 테스트코드 구현

---
+++ src/test/java/com/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.study.datajpa.repository;
+
+import com.study.datajpa.entity.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    public void testMember(){
+        Member member = new Member("테스트2_by Interface Repository");
+        memberRepository.save(member);
+        Member find = memberRepository.findById(member.getId()).get();
+        Assertions.assertThat(find).isEqualTo(member);
+    }
+}
