a059538266abe28e33ca81fa05ec7d43d286c974
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-29 15:14:01 +0900

    feat: Member 멤버 도메인 생성

---
+++ src/main/java/com/study/datajpa/entity/Member.java
@@ -0,0 +1,16 @@
+package com.study.datajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "MEMBER")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
+    private Long id;
+    private String username;
+}
