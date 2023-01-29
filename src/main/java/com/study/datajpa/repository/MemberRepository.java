cdd33b3775cd96f328610765f861d8c1e7bf0b33
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-29 20:52:49 +0900

    feat: MemberRepository 페이징을 하기 위한 Page<>을 이용한 페이징처리 메서드 구현

--- src/main/java/com/study/datajpa/repository/MemberRepository.java
+++ src/main/java/com/study/datajpa/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package com.study.datajpa.repository;
 
 import com.study.datajpa.domain.Member;
 import com.study.datajpa.dto.MemberDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -21,4 +23,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select new com.study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
     List<MemberDto> findMemberDto();
+
+    Page<Member> findByAge(int age, Pageable pageable);
 }
