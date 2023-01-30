15813483ba1ffb22398ae684a9b8d2eea86d2361
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-30 18:59:49 +0900

    feat: MemberController 로직별로 다르게 페이징 사이즈를 조절하는 기능 구현

--- src/main/java/com/study/datajpa/controller/MemberController.java
+++ src/main/java/com/study/datajpa/controller/MemberController.java
@@ -6,6 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,8 +28,9 @@ public class MemberController {
     }
 
     //pageable일 때, url에서 localhost:8080/members?page=페이지넘버&size=페이지의사이즈 로 호출할 수 있다.
+    //@PageableDefault(size = 페이지당 사이즈)를 넣어주면 한 페이지당 개수를 정해줄 수 있다.
     @GetMapping("/members")
-    public Page<Member> list(Pageable pageable){
+    public Page<Member> list(@PageableDefault(size = 5) Pageable pageable){
         return memberRepository.findAll(pageable);
     }
 
