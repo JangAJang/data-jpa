1ec2bef41cdaecf2988c992dd13bbe2ec7b808df
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-30 19:04:48 +0900

    refactor: MemberController 페이징 처리시 멤버로 반환되던 값을 멤버 dto로 변경

--- src/main/java/com/study/datajpa/controller/MemberController.java
+++ src/main/java/com/study/datajpa/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.study.datajpa.controller;
 
 import com.study.datajpa.domain.Member;
+import com.study.datajpa.dto.MemberDto;
 import com.study.datajpa.repository.MemberRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
@@ -30,8 +31,8 @@ public class MemberController {
     //pageable일 때, url에서 localhost:8080/members?page=페이지넘버&size=페이지의사이즈 로 호출할 수 있다.
     //@PageableDefault(size = 페이지당 사이즈)를 넣어주면 한 페이지당 개수를 정해줄 수 있다.
     @GetMapping("/members")
-    public Page<Member> list(@PageableDefault(size = 5) Pageable pageable){
-        return memberRepository.findAll(pageable);
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable){
+        return memberRepository.findAll(pageable).map(member -> new MemberDto(member.getId(), member.getUsername(), null));
     }
 
     @PostConstruct
