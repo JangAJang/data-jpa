318b39793a3b0e853f4c262837d54de7b845bcc4
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-30 18:46:56 +0900

    feat: BaseTimeEntity BaseEntity에서 생성일과 수정일만을 가지는 BaseTimeEntity 분리

--- src/main/java/com/study/datajpa/domain/BaseTimeEntity.java
+++ src/main/java/com/study/datajpa/domain/BaseTimeEntity.java
@@ -5,7 +5,9 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -14,12 +16,12 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseEntity extends BaseTimeEntity{
+public class BaseTimeEntity {
 
-    @CreatedBy
+    @CreatedDate
     @Column(updatable = false)
-    private String createdBy;
-    @LastModifiedBy
-    private String lastModifiedBy;
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 
 }
